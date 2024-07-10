96d84c187bc234f18ce479f7987968c288231a05
vaasanthini <166931307+vaasanthini@users.noreply.github.com>
2024-07-10 11:52:37 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,14 @@
+public class greatest{
+	public static void main( String[] args){
+		int a=15;
+		int b=20;
+		if(a>b)
+		{
+			System.out.println("a is greatest");
+		}
+		else
+		{
+			System.out.println("b is greatest");
+		}
+	}
+}
