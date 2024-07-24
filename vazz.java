7b9f42e12459d058e7e8f7d11e82ba99d17b7565
vaasanthini <166931307+vaasanthini@users.noreply.github.com>
2024-07-24 10:31:51 +0530

    Add files via upload

---
+++ vazz.java
@@ -0,0 +1,20 @@
+public class vazz {
+	static boolean bool;
+	static byte b;
+	static short s;
+	static int i;
+	static long l;
+	static double d;
+	static char c;
+	
+	public static void main(String args[]){
+		System.out.println("default values of primitive data types are:");
+		System.out.println("boolean:" + bool);
+		System.out.println("byte:" + b);
+		System.out.println("short:" + s);
+		System.out.println("int:" + i);
+		System.out.println("long:" + l);
+		System.out.println("double:" + d);
+		System.out.println("char:" + c);
+	  }
+}
